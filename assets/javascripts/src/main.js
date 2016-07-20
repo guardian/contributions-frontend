8f0e6d9d73a383f9d87b2b23c7ce0b7f1f5bd867
Alex Ware <alex.ware@guardian.co.uk>
2016-07-20 15:36:46 +0100

    Revert "take raven out "
    This reverts commit 30347c0dee289a608617245688e8d2dda7e5db76.

--- assets/javascripts/src/main.js
+++ assets/javascripts/src/main.js
@@ -1,6 +1,7 @@
 require([
     'babel-polyfill',
     'ajax',
+    'src/modules/raven',
     'src/modules/analytics/setup',
     'src/modules/images',
     'src/modules/toggle',
@@ -18,6 +19,7 @@ require([
 ], function(
     b,
     ajax,
+    raven,
     analytics,
     images,
     toggle,
@@ -38,6 +40,7 @@ require([
     console.log('Giraffe js initialising.');
 
     ajax.init({page: {ajaxUrl: ''}});
+    raven.init('https://8ad435f4fefe468eb59b19fd81a06ea9@app.getsentry.com/56405');
 
     analytics.init();
 
