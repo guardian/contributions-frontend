771f35a7c420abcbf52086c7992f6d7667e91a3c
Sam Desborough <sam.desborough@gmail.com>
2016-08-03 16:33:54 +0100

    Button styling

--- assets/javascripts/src/components/pages/Contribution.jsx
+++ assets/javascripts/src/components/pages/Contribution.jsx
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default class Contribution extends React.Component {
     render() {
-        return <p>contribution page</p>;
+        const amounts = [25, 50, 100, 250];
+
+        return <div className="contribute-controls">
+            {amounts.map(amount => <button type="button" tabindex="8" className="contribute-controls__button option-button" data-amount={amount}>£{amount}</button>)}
+            <input type="number" className="contribute-controls__input input-text" placeholder="Other amount" maxlength="10" tabindex="12" />
+        </div>;
     }
 }
