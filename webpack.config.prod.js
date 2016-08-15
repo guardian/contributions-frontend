fdc711ffc1cdea277847670804ad1b13bfef6399
Sam Desborough <sam.desborough@gmail.com>
2016-08-15 10:41:15 +0100

    Fix build;

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -28,6 +28,7 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     presets: ['es2015'],
+                    plugins: ['transform-object-rest-spread'],
                     cacheDirectory: ''
                 }
             },
@@ -38,6 +39,7 @@ module.exports = {
                 loader: 'babel',
                 query: {
                     presets: ['react', 'es2015'],
+                    plugins: ['transform-object-rest-spread'],
                     cacheDirectory: ''
                 }
             }
