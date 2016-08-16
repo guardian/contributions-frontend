6fd9512274de7a55e6b0a9c8919bc8a02429814d
Sam Desborough <sam.desborough@gmail.com>
2016-08-16 16:49:12 +0100

    Remove stuff

--- assets/javascripts/src/main.js
+++ assets/javascripts/src/main.js
@@ -1,28 +1,22 @@
 require([
-    'ajax',
     'src/modules/raven',
     'src/modules/analytics/setup',
     'src/modules/images',
     'src/modules/metrics',
-    'src/modules/abTests',
     'src/modules/contribute',
     'src/modules/stripe'
 ], function(
-    ajax,
     raven,
     analytics,
     images,
     metrics,
-    abTests,
     contribute,
     stripe
 ) {
     'use strict';
 
-    ajax.init({ page: { ajaxUrl: '' } });
     analytics.init();
     metrics.init();
     contribute.init();
-    abTests.init();
     stripe.init();
 });
