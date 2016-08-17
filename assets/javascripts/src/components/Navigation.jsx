b37beb60cf941232a6edcf81a8f98e46f794d1a3
Sam Desborough <sam.desborough@gmail.com>
2016-08-17 16:01:05 +0100

    Tidy

--- assets/javascripts/src/components/Navigation.jsx
+++ assets/javascripts/src/components/Navigation.jsx
@@ -5,16 +5,9 @@ import { PAGES } from 'src/constants';
 
 export default class Navigation extends React.Component {
     render() {
-        const showForward = !this.props.processing &&
-            this.props.page !== PAGES.PAYMENT &&
-            this.props.page !== PAGES.PROCESSING;
-
-        const showBack = !this.props.processing &&
-            this.props.page !== PAGES.CONTRIBUTION &&
-            this.props.page !== PAGES.PROCESSING;
-
-        const showPay = !this.props.processing &&
-            this.props.page === PAGES.PAYMENT;
+        const showForward = !this.props.processing && this.props.page !== PAGES.PAYMENT;
+        const showBack = !this.props.processing && this.props.page !== PAGES.CONTRIBUTION;
+        const showPay = !this.props.processing && this.props.page === PAGES.PAYMENT;
 
         return <div className="contribute-navigation">
           {showBack && <a className="contribute-navigation__back" onClick={this.props.goBack}>Back</a> }
