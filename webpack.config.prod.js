5e28c19fa6c98468f46c4a74b7cad899d5146ee3
Sam Desborough <sam.desborough@gmail.com>
2016-08-18 18:29:17 +0100

    Fix  prod build

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -56,7 +56,6 @@ module.exports = {
         new webpack.DefinePlugin({
             "process.env": { NODE_ENV: JSON.stringify("production") }
         }),
-        new webpack.optimize.OccurrenceOrderPlugin(),
         new webpack.optimize.DedupePlugin()
     ],
 
