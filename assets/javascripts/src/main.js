95eb0acd6b468b524768afe3ffcfdcd2db19d0e2
Sam Desborough <sam.desborough@gmail.com>
2016-08-18 13:48:57 +0100

    Reinstate dropdowns

--- assets/javascripts/src/main.js
+++ assets/javascripts/src/main.js
@@ -3,13 +3,19 @@ require([
     'src/modules/images',
     'src/modules/metrics',
     'src/modules/contribute',
-    'src/modules/stripe'
+    'src/modules/stripe',
+    'src/modules/dropdown',
+    'src/modules/identityPopup',
+    'src/modules/identityPopupDetails'
 ], function (
     analytics,
     images,
     metrics,
     contribute,
-    stripe
+    stripe,
+    dropdown,
+    identityPopup,
+    identityPopupDetails
 ) {
     'use strict';
 
@@ -18,4 +24,8 @@ require([
     metrics.init();
     contribute.init();
     stripe.init();
+
+    dropdown.init();
+    identityPopup.init();
+    identityPopupDetails.init();
 });
