62e417a7e8531f5afa01b85809d4d4e1f096c4aa
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-08-24 14:49:22 +0100

    always display the "secure" label and the padlock

--- assets/javascripts/src/components/Title.jsx
+++ assets/javascripts/src/components/Title.jsx
@@ -15,14 +15,12 @@ export default class Title extends React.Component {
                 {titlesByPageId[this.props.page]}
             </h3>
 
-            {this.props.page === PAGES.PAYMENT &&
-                <div className="security-note">
-                    <svg className="icon-inline icon-inline--small icon-inline--top">
-                        <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#icon-secure"></use>
-                    </svg>
-                    This site is secure
-                </div>
-            }
+            <div className="security-note">
+                <svg className="icon-inline icon-inline--small icon-inline--top">
+                    <use xmlnsXlink="http://www.w3.org/1999/xlink" xlinkHref="#icon-secure"></use>
+                </svg>
+                Secure
+            </div>
 
         </div>;
     }
