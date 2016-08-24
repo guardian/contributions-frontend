c7bb13d705f134c0f98f7bb0e3c538a3b0f5a883
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-08-24 16:20:45 +0100

    remove the optional attribute and use the native required attribute

--- assets/javascripts/src/components/InputField.jsx
+++ assets/javascripts/src/components/InputField.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 export default class InputField extends React.Component {
     render() {
-        const { outerClassName, children, optional, ...props } = this.props;
+        const { outerClassName, children, ...props } = this.props;
 
         return <div className={outerClassName || ''}>
             <label htmlFor={this.props.id} className="label">{this.props.label}
-                {optional && <span className="label-optional">(optional)</span>}
+                {!this.props.required && <span className="label-optional">(optional)</span>}
             </label>
             <input {...props} className={'input-text contribute-controls__input ' + (props.className || '')} />
             {children}
--- assets/javascripts/src/components/pages/Details.jsx
+++ assets/javascripts/src/components/pages/Details.jsx
@@ -16,7 +16,7 @@ export default class Details extends React.Component {
                         onChange={ event => this.props.updateDetails({ email: event.target.value })}
                         required />
 
-            <InputField label="Postcode" id="postcode" type="text" optional="true"
+            <InputField label="Postcode" id="postcode" type="text"
                         value={this.props.details.postcode}
                         onChange={ event => this.props.updateDetails({ postcode: event.target.value })}
                         />
