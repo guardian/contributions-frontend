8049e14b292fa812021b01c8159d60cf7f0c2c72
Alex Ware <alex.ware@guardian.co.uk>
2016-09-16 13:38:02 +0100

    fix tab order
    handle focus for cards
    
    some tidying up and new hover state for action secondary
    
    new state for focus only

--- assets/javascripts/src/components/pages/Payment.jsx
+++ assets/javascripts/src/components/pages/Payment.jsx
@@ -6,6 +6,7 @@ import InputField from '../InputField';
 import CardIcon from '../CardIcon';
 
 export default class Payment extends React.Component {
+
     formatExpiry(expiry) {
         if (expiry.length >= 3 && expiry.indexOf('/') === -1) {
             return expiry.slice(0, 2) + '/' + expiry.slice(2);
@@ -46,45 +47,45 @@ export default class Payment extends React.Component {
         return <div className='contribute-payment contribute-fields'>
 
             <InputField label="Card number" type="text" value={formatCardNumber(this.props.card.number)}
-                        onChange={event => this.props.updateCard({ number: event.target.value })}
+                        autoFocus={window.matchMedia('(min-width: 741px)').matches}
+                        onChange={event => this.props.updateCard({number: event.target.value})}
                         onKeyDown={event => this.clearValidation(event.target)}
                         onBlur={this.validateCardNumber.bind(this)}
-                        tabIndex="13" size="20" id="cc-num" data-stripe="number"
+                        size="20" id="cc-num" data-stripe="number"
                         pattern="[0-9 ]*" placeholder="0000 0000 0000 0000" maxLength="19" autoComplete="off"
-                        outerClassName="with-card-icon" required>
-                <CardIcon number={this.props.card.number} />
+                        outerClassName="with-card-icon"
+                        required>
+                <CardIcon number={this.props.card.number}/>
             </InputField>
 
             <div className="flex-horizontal">
                 <InputField label="Expiry date"
                             type="text"
                             value={this.props.card.expiry}
-                            onChange={event => this.props.updateCard({ expiry: this.formatExpiry(event.target.value) })}
+                            onChange={event => this.props.updateCard({expiry: this.formatExpiry(event.target.value)})}
                             onKeyDown={event => this.clearValidation(event.target)}
                             onBlur={this.validateExpiry.bind(this)}
                             className="center-text"
                             outerClassName="half-width"
                             placeholder="MM/YY"
                             maxLength="5"
-                            tabIndex="14"
-                            required />
+                            required/>
 
                 <InputField label="Security code"
                             type="text"
                             value={this.props.card.cvc}
-                            onChange={event => this.props.updateCard({ cvc: event.target.value })}
+                            onChange={event => this.props.updateCard({cvc: event.target.value})}
                             onKeyDown={event => this.clearValidation(event.target)}
                             onBlur={this.validateCVC.bind(this)}
                             className="center-text"
                             outerClassName="half-width"
-                            tabIndex="15"
-                            required />
+                            required/>
             </div>
 
             {this.props.error.show &&
-                <div className="payment-error">
-                    {this.props.error.kind === 'card' ? this.props.error.message : 'Sorry, an error occurred, please try again.'}
-                </div>
+            <div className="payment-error">
+                {this.props.error.kind === 'card' ? this.props.error.message : 'Sorry, an error occurred, please try again.'}
+            </div>
             }
 
         </div>
