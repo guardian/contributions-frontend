c40fb7fe5ec19b83e4c17e3929cc45b7f849e317
Sam Desborough <sam.desborough@gmail.com>
2016-09-21 17:49:53 +0100

    Remove unused param

--- assets/javascripts/src/components/Main.jsx
+++ assets/javascripts/src/components/Main.jsx
@@ -98,7 +98,7 @@ class Main extends React.Component {
         }
     }
 
-    submit(mobile, event) {
+    submit(event) {
         event.preventDefault(); // we never want the standard submit behaviour, which triggers a reload
 
         if (!event.target.checkValidity()) return;
@@ -125,7 +125,7 @@ class Main extends React.Component {
 
             <MediaQuery query='(min-width: 741px)'>
                 {this.renderSummary(showSummary)}
-                <DesktopWrapper submit={this.submit.bind(this, false)} componentFor={this.componentFor.bind(this)} {...this.props} />
+                <DesktopWrapper submit={this.submit.bind(this)} componentFor={this.componentFor.bind(this)} {...this.props} />
             </MediaQuery>
         </div>
     }
