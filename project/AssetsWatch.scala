35978ca655f5da5d0c1dba8bc9d5ac81bd3628dc
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-10-12 14:40:42 +0100

    keep a global state just in case something goes wrong

--- project/AssetsWatch.scala
+++ project/AssetsWatch.scala
@@ -3,20 +3,25 @@ import sbt._
 import java.net.InetSocketAddress
 
 object AssetsWatch {
+
+  var gruntWatch: Option[Process] = None
+  var webpackWatch: Option[Process] = None
+
+  def killall(): Unit = {
+    gruntWatch.foreach(_.destroy())
+    gruntWatch = None
+    webpackWatch.foreach(_.destroy())
+    webpackWatch = None
+  }
+
   def apply(base: File): PlayRunHook = new PlayRunHook {
-    var gruntWatch: Option[Process] = None
-    var webpackWatch: Option[Process] = None
 
     override def afterStarted(addr: InetSocketAddress): Unit = {
+      killall()
       gruntWatch = Some(Process("npm run watchGrunt", base).run)
       webpackWatch = Some(Process("npm run watchWebpack", base).run)
     }
 
-    override def afterStopped(): Unit = {
-      gruntWatch.foreach(_.destroy())
-      gruntWatch = None
-      webpackWatch.foreach(_.destroy())
-      webpackWatch = None
-    }
+    override def afterStopped(): Unit = killall()
   }
 }
