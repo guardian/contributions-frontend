abfc79538ebf1867fbecc2694aa088bd5c19c35e
Alexandre Dufournet <alex.dufournet@theguardian.com>
2016-10-19 10:48:23 +0100

    Add contributor id and updated timestamp on the contributor table

--- app/models/Contributor.scala
+++ app/models/Contributor.scala
@@ -1,7 +1,16 @@
 package models
 
+import java.util.UUID
+
+case class ContributorId(id: UUID) extends AnyVal
+
+object ContributorId {
+  def random: ContributorId = ContributorId(UUID.randomUUID())
+}
+
 case class Contributor(
   email: String,
+  contributorId: Option[ContributorId],
   name: Option[String],
   firstName: Option[String],
   lastName: Option[String],
