d05aeeff94e1d052ab7bdd176f9904726e6f1697
Philip Wills <philip.wills@guardian.co.uk>
2016-11-07 16:46:45 +0000

    Minimal change to disable Stripe
    In case of emergency, remove button

--- assets/javascripts/src/components/Navigation.jsx
+++ assets/javascripts/src/components/Navigation.jsx
@@ -29,7 +29,6 @@ export default class Navigation extends React.Component {
         {showForward && <Forward className="contribute-navigation__button contribute-navigation__next hidden-mobile">Next</Forward>}
         {showPay && <Forward className='contribute-navigation__button action--pay' onClick={this.props.payWithCard}>Pay {this.props.currency.prefix}{this.props.currency.symbol}{this.props.amount}</Forward>}
         {showMobileBack && <Back className="action--secondary contribute-navigation__back show-mobile" onClick={this.props.jumpToFirstPage}>Back</Back>}
-        {isFirstPage &&  <Forward className="contribute-navigation__button action action--button contribute-navigation__next action--next contribute_card__button" onClick={this.props.clearPaymentFlags}>{"Contribute with debit/credit card"}</Forward>}
         {isFirstPage && <Forward className="contribute-navigation__button action action--button  paypal__button" onClick={this.props.payWithPaypal}>Contribute with</Forward>}
         {processing && <Spinner text="Processing" />}
         </div>;
