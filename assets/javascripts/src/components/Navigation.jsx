7b07d4ada581d7013a11579fd9bc7edf52ee6955
Alex Ware <alex.ware@guardian.co.uk>
2017-01-23 12:29:35 +0000

    refactor nav

--- assets/javascripts/src/components/Navigation.jsx
+++ assets/javascripts/src/components/Navigation.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import Spinner from './Spinner';
-import { Back, Forward } from './Buttons';
-import { PAGES } from 'src/constants';
+import {Back, Forward} from './Buttons';
+import {PAGES} from 'src/constants';
 
 export default class Navigation extends React.Component {
     classNameFor(page) {
-        switch(page) {
+        switch (page) {
             case PAGES.CONTRIBUTION:
                 return 'contribution';
             case PAGES.DETAILS:
@@ -22,16 +22,42 @@ export default class Navigation extends React.Component {
         const showMobileBack = !processing && this.props.page == PAGES.PAYMENT;
         const showBack = !processing && this.props.page !== PAGES.CONTRIBUTION;
         const showPay = !processing && !!this.props.amount && this.props.page === PAGES.PAYMENT;
-        const showForward = !processing && this.props.page == PAGES.DETAILS;
+        const showNext = !processing && this.props.page == PAGES.DETAILS;
+        const classes = {
+            nav: () => {'contribute-navigation ' + this.classNameFor(this.props.page)},
+            desktop: {
+                back: 'action--secondary contribute-navigation__back hidden-mobile',
+                next: 'contribute-navigation__button contribute-navigation__next hidden-mobile'
+            },
+            mobile: {back: 'action--secondary contribute-navigation__back show-mobile'},
+            pay: 'contribute-navigation__button action--pay',
+            ccnext: 'contribute-navigation__button action action--button contribute-navigation__next action--next contribute_card__button',
+            paypal: 'contribute-navigation__button action action--button  paypal__button'
+        };
+        const amount = this.props.currency.prefix + this.props.currency.symbol + this.props.amount;
 
-        return <div className={'contribute-navigation ' + this.classNameFor(this.props.page)}>
-        {showBack && <Back type="button" className="action--secondary contribute-navigation__back hidden-mobile" onClick={this.props.goBack}>Back</Back>}
-        {showForward && <Forward className="contribute-navigation__button contribute-navigation__next hidden-mobile">Next</Forward>}
-        {showPay && <Forward className='contribute-navigation__button action--pay' onClick={this.props.payWithCard}>Pay {this.props.currency.prefix}{this.props.currency.symbol}{this.props.amount}</Forward>}
-        {showMobileBack && <Back className="action--secondary contribute-navigation__back show-mobile" onClick={this.props.jumpToFirstPage}>Back</Back>}
-        {isFirstPage &&  <Forward className="contribute-navigation__button action action--button contribute-navigation__next action--next contribute_card__button" onClick={this.props.clearPaymentFlags}>{"Contribute with debit/credit card"}</Forward>}
-        {isFirstPage && <Forward className="contribute-navigation__button action action--button  paypal__button" onClick={this.props.payWithPaypal}>Contribute with</Forward>}
-        {processing && <Spinner text="Processing" />}
+        return <div className={classes.nav()}>
+            {showBack && <Back type="button"
+                               className={classes.desktop.back}
+                               onClick={this.props.goBack}>Back</Back>
+            }
+            {showNext &&
+            <Forward className={classes.desktop.next}>Next</Forward>
+            }
+            {showPay && <Forward className={classes.pay}
+                                 onClick={this.props.payWithCard}>Pay {amount}</Forward>
+            }
+            {showMobileBack && <Back className={classes.mobile.back}
+                                     onClick={this.props.jumpToFirstPage}>Back</Back>
+            }
+            {isFirstPage && <Forward className={classes.cc}
+                                       onClick={this.props.clearPaymentFlags}
+                                              >{"Contribute with debit/credit card"}</Forward>
+            }
+            {isFirstPage && <Forward className={classes.paypal}
+                                     onClick={this.props.payWithPaypal}>Contribute with</Forward>
+            }
+            {processing && <Spinner text="Processing"/>}
         </div>;
     }
 }
