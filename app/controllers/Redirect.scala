fda19ece779ad43922c47c3b00e0452b3d24bdbe
Max Spencer <mrmaxspencer@googlemail.com>
2017-02-06 15:35:39 +0000

    Add new queryParamsToForward for apps
    As per this JIRA card: https://theguardian.atlassian.net/browse/AND-11445

--- app/controllers/Redirect.scala
+++ app/controllers/Redirect.scala
@@ -5,7 +5,7 @@ import play.api.mvc.{Controller, Request}
 trait Redirect {
   self: Controller =>
   def redirectWithCampaignCodes(destinationUrl: String, additionalParams: Set[String] = Set.empty)(implicit request: Request[Any]) = {
-    val queryParamsToForward = Set("INTCMP", "CMP", "REFPVID") ++ additionalParams
+    val queryParamsToForward = Set("INTCMP", "CMP", "REFPVID", "app_id", "platform", "app_version") ++ additionalParams
     Redirect(destinationUrl, request.queryString.filterKeys(queryParamsToForward), SEE_OTHER)
   }
 }
