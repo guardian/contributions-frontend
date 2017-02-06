a5ad0f20811bc29d5b5ec387f9477cf8e4bc94e3
Sam Desborough <sam.desborough@gmail.com>
2017-02-06 17:24:40 +0000

    Improve segmentation logic and add test

--- app/abtests/Test.scala
+++ app/abtests/Test.scala
@@ -1,37 +1,44 @@
 package abtests
 
-import com.github.slugify._
+import com.github.slugify.Slugify
 import play.api.libs.json.{JsValue, Json, Writes}
 import play.api.mvc.Cookie
 
-case class Test(name: String, audienceSize: Double, audienceOffset: Double, variants: Variant*) {
-  private val startId: Int = (audienceOffset * Test.maxTestId).toInt
-  private val endId: Int = (startId + (audienceSize * Test.maxTestId)).toInt
-  private val idRange: Range = startId.to(endId).tail
+import scala.util.Try
+
+case class Percentage(value: Double) {
+  def of[B](x: B)(implicit numB: Numeric[B]): Double = numB.toDouble(x) / 100 * value
+}
+
+case class Test(name: String, audienceSize: Percentage, audienceOffset: Percentage, variants: Variant*) {
+  val startId: Int = audienceOffset.of(Test.maxTestId).toInt
+  val endId: Int = startId + audienceSize.of(Test.maxTestId).toInt
+  val idRange: Range = startId.to(endId).tail
 
   val slug: String = Test.slugify(name)
   val cookieName: String = s"${Test.cookiePrefix}.${slug}"
 
   def allocate(id: Int): Option[Allocation] = {
-    idRange.grouped(idRange.length / variants.size)
-      .zip(variants.iterator)
-      .find { case (ids, _) => id >= ids.head && id <= ids.last }
-      .map(t => Allocation(this, t._2))
+    if (!idRange.contains(id)) None
+    else Try(variants.toList(id % variants.size)).toOption.map(v => Allocation(this, v))
   }
 }
 
 object Test {
+  implicit class PercentageNum[A](x: A)(implicit num: Numeric[A]) {
+    val percent = Percentage(num.toDouble(x))
+  }
+
   private val slugifier = new Slugify()
 
-  val maxTestId = 100
-  val cookiePrefix = "gu.contributions.test"
-  val testIdCookieName = s"$cookiePrefix.id"
+  val maxTestId: Int = 10000
+  val cookiePrefix = "gu.contributions.ab"
+  val testIdCookieName: String = s"$cookiePrefix.id"
 
-  val stripeTest = Test("Stripe checkout", 0.3, 0, Variant("control"), Variant("stripe"))
-  val allTests = Set(stripeTest)
+  val stripeTest = Test("Stripe checkout", 30.percent, 0.percent, Variant("control"), Variant("stripe"))
+  val allTests: Set[Test] = Set(stripeTest)
 
   def slugify(s: String): String = slugifier.slugify(s)
-
   def idCookie(id: Int) = Cookie(testIdCookieName, id.toString, maxAge = Some(604800))
   def allocations(id: Int): Set[Allocation] = allTests.flatMap(_.allocate(id))
 }
