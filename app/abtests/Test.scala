0b560004a6162b1d8d48392c935459f6a5985b72
Guy Dawson <gccdawson@gmail.com>
2017-06-05 17:38:52 +0100

    Landing page test update (#282)
    * update which means readers coming from thrasher will see epic-like landing page
    
    * re-arrange ifelse statement to give this land precedent

--- app/abtests/Test.scala
+++ app/abtests/Test.scala
@@ -46,7 +46,7 @@ object Test {
 
   val stripeTest = Test("Stripe checkout", 100.percent, 0.percent, Seq(Variant("stripe")))
 
-  val landingPageTest = Test("Landing page", 100.percent, 0.percent, Seq(Variant("with-copy")), cmpCheck("gdnwb_copts.*_banner".r))
+  val landingPageTest = Test("Landing page", 100.percent, 0.percent, Seq(Variant("with-copy")), cmpCheck("(gdnwb_copts(.*_banner)|(.*_thrasher))".r))
 
   val allTests: Set[Test] = Set(stripeTest, landingPageTest)
 
