6bed694c48c431c557bfa813eaf4cde5141cec52
Sam Desborough <sam.desborough@gmail.com>
2017-06-21 11:55:58 +0100

    Add cache header to inline payment form

--- app/controllers/EpicComponentsController.scala
+++ app/controllers/EpicComponentsController.scala
@@ -5,9 +5,11 @@ import play.api.mvc.Action
 import play.api.mvc.AnyContent
 import play.api.mvc.Controller
 
+import scala.concurrent.duration._
+
 class EpicComponentsController extends Controller {
 
-  def inlinePayment: Action[AnyContent] = NoCacheAction { implicit request =>
-    Ok(views.html.epicComponents.inlinePayment())
+  def inlinePayment: Action[AnyContent] = Action { implicit request =>
+    Cached(7.days.toSeconds.toInt)(Ok(views.html.epicComponents.inlinePayment()))
   }
 }
