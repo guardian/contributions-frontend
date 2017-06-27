6909eeffea4d3b15b38b45786f2bb95872775d3f
Sam Desborough <sam.desborough@gmail.com>
2017-06-27 17:19:00 +0100

    Style error message

--- assets/javascripts/src/components/inline-payment/ThankYouMessage.jsx
+++ assets/javascripts/src/components/inline-payment/ThankYouMessage.jsx
@@ -2,9 +2,11 @@ import { h } from 'preact';
 /** @jsx h */
 
 const ThankYouMessage = props => (
-    <h1>
-        Thank you!
-    </h1>
+    <div class="thank-you">
+        <h2 class="thank-you__heading">Thank you!</h2>
+        <p>You’ve made a vital contribution that will help us maintain our indepedent, investigative journalism.</p>
+        <p>If you have any questions about contributing to the Guardian, please <a href="mailto:contribution.support@theguardian.com">contact us</a>.</p>
+    </div>
 );
 
 export default ThankYouMessage;
