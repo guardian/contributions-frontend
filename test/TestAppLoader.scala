b4c140480819a9b7c5a0a9b74e004925217aedf2
Sam Desborough <sam.desborough@gmail.com>
2017-07-19 14:09:07 +0100

    Create TestAppLoader and simplify test code

--- test/TestAppLoader.scala
+++ test/TestAppLoader.scala
@@ -1,17 +1,49 @@
+import cats.data.EitherT
+import com.gu.i18n.GBP
+import com.paypal.api.payments.Payment
+import models.ContributionAmount
+import monitoring.{CloudWatchMetrics, LoggingTags}
+import org.mockito.{Matchers, Mockito}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play.FakeApplicationFactory
 import play.api._
+import play.api.mvc.Request
 import play.core.DefaultWebCommands
-import services.EmailService
+import services.{EmailService, PaymentServices, PaypalService}
 import wiring.AppComponents
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.{ExecutionContext, Future}
 
 trait TestComponents extends MockitoSugar {
   self: AppComponents =>
+  import cats.instances.future._
+
+  val mockPaypalService = mock[PaypalService]
+  val mockPaypalPayment: Payment = Mockito.mock[Payment](classOf[Payment], Mockito.RETURNS_DEEP_STUBS)
+  val mockContributionAmount: ContributionAmount = ContributionAmount(10, GBP)
 
   override lazy val emailService: EmailService = mock[EmailService]
   override val jdbcExecutionContext: ExecutionContext = scala.concurrent.ExecutionContext.global
+  override lazy val cloudWatchMetrics: CloudWatchMetrics = mock[CloudWatchMetrics]
+  override lazy val paymentServices: PaymentServices = mock[PaymentServices]
+
+  Mockito.when(paymentServices.paypalServiceFor(Matchers.any[Request[_]]))
+    .thenReturn(mockPaypalService)
+
+  Mockito.when(mockPaypalService.paymentAmount(mockPaypalPayment))
+    .thenReturn(Some(mockContributionAmount))
+
+  Mockito.when(mockPaypalPayment.getCreateTime)
+    .thenReturn("")
+
+  Mockito.when(mockPaypalPayment.getPayer.getPayerInfo.getEmail)
+    .thenReturn("a@b.com")
+
+  Mockito.when(mockPaypalService.executePayment(Matchers.eq("PASS"), Matchers.eq("PASS"))(Matchers.any[LoggingTags]))
+    .thenReturn(EitherT.right[Future, String, Payment](Future.successful(mockPaypalPayment)))
+
+  Mockito.when(mockPaypalService.executePayment(Matchers.eq("FAIL"), Matchers.eq("FAIL"))(Matchers.any[LoggingTags]))
+    .thenReturn(EitherT.left[Future, String, Payment](Future.successful("failed")))
 }
 
 trait TestApplicationFactory extends FakeApplicationFactory {
--- test/controllers/PaypalControllerSpec.scala
+++ test/controllers/PaypalControllerSpec.scala
@@ -1,20 +1,9 @@
-import cats.data.EitherT
-import com.gu.i18n.{CountryGroup, GBP}
-import com.paypal.api.payments.Payment
 import controllers.PaypalController
-import models.ContributionAmount
-import monitoring.{CloudWatchMetrics, LoggingTags}
-import org.mockito.{Matchers, Mockito}
 import org.scalatest.mockito.MockitoSugar
 import org.scalatestplus.play._
 import play.api.mvc._
-import play.api.test.Helpers._
+import play.api.test.Helpers.{redirectLocation, _}
 import play.api.test._
-import play.filters.csrf.CSRFCheck
-import services.{PaymentServices, PaypalService}
-
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.Future
 
 class PaypalControllerSpec extends PlaySpec
   with Results
@@ -22,70 +11,22 @@ class PaypalControllerSpec extends PlaySpec
   with BaseOneAppPerTest
   with TestApplicationFactory {
 
-  import cats.instances.future._
+  def fakeGET(path: String): FakeRequest[AnyContentAsEmpty.type] =
+    FakeRequest(GET, path).withHeaders(("Accept", "text/html"))
 
-  val mockCSRFCheck: CSRFCheck = mock[CSRFCheck]
-  val mockCloudWatchMetrics: CloudWatchMetrics = mock[CloudWatchMetrics]
-  val mockPaymentServices: PaymentServices = mock[PaymentServices]
-  val mockPaypalService: PaypalService = mock[PaypalService]
-  val mockPaypalPayment: Payment = Mockito.mock[Payment](classOf[Payment], Mockito.RETURNS_DEEP_STUBS)
-  val mockLoggingTags: LoggingTags = mock[LoggingTags]
-  val mockContributionAmount: ContributionAmount = ContributionAmount(10, GBP)
-
-  val fakeHeaders: FakeHeaders = FakeHeaders(Seq(("Accept", "text/html")))
-  val fakeRequest: Request[AnyContent] = FakeRequest("GET", "/", fakeHeaders, AnyContentAsEmpty)
-
-  Mockito.when(mockPaymentServices.paypalServiceFor(Matchers.any[Request[_]]))
-    .thenReturn(mockPaypalService)
-
-  Mockito.when(mockPaypalPayment.getCreateTime)
-    .thenReturn("")
-
-  Mockito.when(mockPaypalService.paymentAmount(mockPaypalPayment))
-    .thenReturn(Some(mockContributionAmount))
-
-  Mockito.when(mockPaypalPayment.getPayer.getPayerInfo.getEmail)
-    .thenReturn("a@b.com")
-
-  WsTestClient.withClient { client =>
-
-    val controller = new PaypalController(client, mockPaymentServices, mockCSRFCheck, mockCloudWatchMetrics)
-    val executePayment = controller.executePayment(
-      countryGroup = CountryGroup.UK,
-      paymentId = "1",
-      token = "hello",
-      payerId = "YO",
-      cmp = None,
-      intCmp = None,
-      refererPageviewId = None,
-      refererUrl = None,
-      ophanPageviewId = None,
-      ophanBrowserId = None,
-      ophanVisitId = None
-    )
-
-    "Paypal Controller" should {
-
-      "generate correct redirect URL for successful PayPal payments" in {
-        Mockito.when(mockPaypalService.executePayment(Matchers.anyString, Matchers.anyString)(Matchers.any[LoggingTags]))
-          .thenReturn(EitherT.right[Future, String, Payment](Future.successful(mockPaypalPayment)))
-
-        val result = executePayment.apply(fakeRequest)
-
+  "Paypal Controller" should {
+    "generate correct redirect URL for successful PayPal payments" in {
+      route(app, fakeGET("/paypal/uk/execute?paymentId=PASS&PayerID=PASS&token=123")).foreach { result =>
         status(result).mustBe(303)
         redirectLocation(result).mustBe(Some("/uk/post-payment"))
       }
+    }
 
-      "generate correct redirect URL for unsuccessful PayPal payments" in {
-        Mockito.when(mockPaypalService.executePayment(Matchers.anyString, Matchers.anyString)(Matchers.any[LoggingTags]))
-          .thenReturn(EitherT.left[Future, String, Payment](Future.successful("failed")))
-
-        val result = executePayment.apply(fakeRequest)
-
+    "generate correct redirect URL for unsuccessful PayPal payments" in {
+      route(app, fakeGET("/paypal/uk/execute?paymentId=FAIL&PayerID=FAIL&token=123")).foreach { result =>
         status(result).mustBe(303)
         redirectLocation(result).mustBe(Some("/uk?error_code=PaypalError"))
       }
     }
-
   }
 }
