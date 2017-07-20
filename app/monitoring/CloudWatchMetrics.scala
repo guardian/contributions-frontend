91f04d4e9b250362246d3acccbdb0cd2b6410da8
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-07-20 17:03:58 +0100

    fixing up Cloudwatch metric dimensions format

--- app/monitoring/CloudWatchMetrics.scala
+++ app/monitoring/CloudWatchMetrics.scala
@@ -2,108 +2,112 @@ package monitoring
 
 import com.amazonaws.services.cloudwatch.AmazonCloudWatchAsync
 import com.amazonaws.services.cloudwatch.model.{Dimension, MetricDatum, PutMetricDataRequest}
-import com.gu.zuora.soap.models.Commands.PaymentMethod
 import configuration.Config
 import models.PaymentProvider
-import play.api.libs.json.Json
-
-import scala.tools.nsc.backend.Platform
 
 
 /**
   * Created by mmcnamara on 12/07/2017.
   */
 class CloudWatchMetrics(cloudWatchClient: AmazonCloudWatchAsync) extends TagAwareLogger {
+  import CloudWatchMetrics.DimensionValue
 
   val stage: String = Config.stage
   val application = "contributions" // This sets the namespace for Custom Metrics in AWS (see CloudWatch)
 
-  val stageDimension: Dimension = new Dimension().withName("Stage").withValue(stage)
+  val stageDimension: Dimension = new Dimension().withName("stage").withValue(stage)
 
-  def put(name: String, paymentProvider: Option[PaymentProvider], platform: String): Unit = {
+  private def put(name: String, paymentProvider: String, platform: String): Unit = {
     val platformDimension = new Dimension().withName("platform").withValue(platform)
+    val paymentProviderDimension = new Dimension().withName("payment-provider").withValue(paymentProvider)
 
     val metric =
       new MetricDatum()
         .withValue(1d)
         .withMetricName(name)
         .withUnit("Count")
-        .withDimensions(stageDimension, platformDimension)
-
-
-    paymentProvider.foreach { provider =>
-      val paymentMethodDimension = new Dimension().withName("paymentMethod").withValue(provider.entryName)
-      metric.setDimensions(java.util.Arrays.asList(paymentMethodDimension))
-    }
+        .withDimensions(stageDimension, platformDimension, paymentProviderDimension)
 
     val request = new PutMetricDataRequest().withNamespace(application).withMetricData(metric)
 
     cloudWatchClient.putMetricDataAsync(request, CloudWatch.LoggingAsyncHandler)
   }
 
+  private def put(name: String, paymentProvider: PaymentProvider, platform: String): Unit = {
+    put(name, paymentProvider.entryName, platform)
+  }
 
   def logHomePage(platform: String): Unit = {
-    put("home-page-displayed", None, platform)
+    put("home-page-displayed", DimensionValue.notApplicable, platform)
   }
 
   def logPaymentAuthAttempt(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-authorisation-attempt", Some(paymentProvider), platform)
+    put("payment-authorisation-attempt", paymentProvider.entryName, platform)
   }
 
   def logPaymentAuthSuccess(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-authorisation-success", Some(paymentProvider), platform)
+    put("payment-authorisation-success", paymentProvider, platform)
   }
 
   def logPaymentAuthFailure(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-authorisation-failure", Some(paymentProvider), platform)
+    put("payment-authorisation-failure", paymentProvider, platform)
   }
 
   def logPaymentAttempt(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-attempt", Some(paymentProvider), platform)
+    put("payment-attempt", paymentProvider, platform)
   }
 
   def logPaymentSuccess(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-success",Some(paymentProvider), platform)
+    put("payment-success", paymentProvider, platform)
   }
 
   def logPaymentSuccessRedirected(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-success-redirected-to-other-platform", Some(paymentProvider), platform)
+    put("payment-success-redirected-to-other-platform", paymentProvider, platform)
   }
 
   def logPaymentFailure(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-failure", Some(paymentProvider), platform)
+    put("payment-failure", paymentProvider, platform)
   }
 
   def logHookAttempt(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-hook-attempt", Some(paymentProvider), platform)
+    put("payment-hook-attempt", paymentProvider, platform)
   }
 
   def logHookParsed(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-hook-parsed", Some(paymentProvider), platform)
+    put("payment-hook-parsed", paymentProvider, platform)
   }
 
   def logHookInvalidRequest(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("contribution-paypal-hook-invalid-request", Some(paymentProvider), platform)
+    put("contribution-paypal-hook-invalid-request", paymentProvider, platform)
   }
 
   def logHookParseError(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-hook-parse-error", Some(paymentProvider), platform)
+    put("payment-hook-parse-error", paymentProvider, platform)
   }
 
   def logHookProcessed(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-hook-processed", Some(paymentProvider), platform)
+    put("payment-hook-processed", paymentProvider, platform)
   }
 
   def logHookProcessError(paymentProvider: PaymentProvider, platform: String): Unit = {
-    put("payment-hook-processing-error", Some(paymentProvider), platform)
+    put("payment-hook-processing-error", paymentProvider, platform)
   }
 
   def logPostPaymentPageDisplayed(paymentProvider: Option[PaymentProvider], platform: String): Unit = {
-    put("post-payment-page-displayed", paymentProvider, platform)
+    put("post-payment-page-displayed", paymentProvider.map(_.entryName).getOrElse(DimensionValue.unknown), platform)
   }
 
   def logThankYouPageDisplayed(paymentProvider: Option[PaymentProvider], platform: String): Unit = {
-    put("thank-you-page-displayed", paymentProvider, platform)
+    put("thank-you-page-displayed", paymentProvider.map(_.entryName).getOrElse(DimensionValue.unknown), platform)
   }
 
+}
+
+
+object CloudWatchMetrics {
+
+  object DimensionValue {
+    val unknown = "unknown"
+    val notApplicable = "na"
+  }
 }
