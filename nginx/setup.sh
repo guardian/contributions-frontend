4631a3152f3b71138ef1e22d31c51565e12b4a9b
Sam Desborough <sam.desborough@gmail.com>
2017-07-24 15:16:56 +0100

    Use wildcard certificate for contribute.thegulocal.com
    The previous cert has expired, this one is valid until 2019

--- nginx/setup.sh
+++ nginx/setup.sh
@@ -6,13 +6,9 @@ NGINX_HOME=$(nginx -V 2>&1 | grep 'configure arguments:' | sed 's#.*conf-path=\(
 
 sudo mkdir -p ${NGINX_HOME}/sites-enabled
 sudo ln -fs ${DIR}/contributions.conf ${NGINX_HOME}/sites-enabled/contributions.conf
-aws s3 cp s3://identity-local-ssl/contribute-thegulocal-com-exp2017-07-19-bundle.crt ${GU_KEYS}/ --profile membership
-aws s3 cp s3://identity-local-ssl/contribute-thegulocal-com-exp2017-07-19.key ${GU_KEYS}/ --profile membership
 
-aws s3 cp s3://identity-local-ssl/wildcard-thegulocal-com-exp2019-01-09.crt ${NGINX_HOME} --profile membership
-
-aws s3 cp s3://identity-local-ssl/wildcard-thegulocal-com-exp2019-01-09.key ${NGINX_HOME} --profile membership
-
+aws s3 cp s3://identity-local-ssl/wildcard-thegulocal-com-exp2019-01-09.crt ${GU_KEYS} --profile membership
+aws s3 cp s3://identity-local-ssl/wildcard-thegulocal-com-exp2019-01-09.key ${GU_KEYS} --profile membership
 
 sudo ln -fs ${GU_KEYS}/ ${NGINX_HOME}/keys
 
