5b531ba1dd043e630e5eda08a129c1ef3380fc34
Jonathan Rankin <jonathan.rankin@guardian.co.uk>
2017-07-27 11:53:09 +0100

    selenium tests implemented

--- test/acceptance/util/Browser.scala
+++ test/acceptance/util/Browser.scala
@@ -15,6 +15,9 @@ trait Browser extends WebBrowser {
   def pageHasElement(q: Query): Boolean =
     waitUntil(ExpectedConditions.visibilityOfElementLocated(q.by))
 
+  def pageDoesNotHaveElement(q: Query): Boolean =
+    waitUntil(ExpectedConditions.not(ExpectedConditions.presenceOfAllElementsLocatedBy(q.by)))
+
   def clickOn(q: Query) {
     if (pageHasElement(q))
       click.on(q)
@@ -28,6 +31,41 @@ trait Browser extends WebBrowser {
 
       if (element.nonEmpty) element.foreach(_.click)
       else throw new Exception(s"Could not find ${n}th element for query ${q.queryString}")
+    }
+  }
+
+
+  def setValue(q: Query, value: String, clear: Boolean = false) {
+    if (pageHasElement(q)) {
+
+      if (clear) q.webElement.clear
+      q.webElement.sendKeys(value)
+
+    } else
+      throw new Exception(s"Could not fill in details for query ${q.queryString}")
+  }
+
+  // Switches to a new iframe specified by the Query, q.
+  def switchFrame(q: Query) {
+    if (pageHasElement(q))
+      driver.switchTo().frame(q.webElement)
+    else
+      throw new Exception(s"Could not switch to the frame specified by the following query ${q.queryString}")
+  }
+
+  /*
+  * Stripe wants you to pause between month and year and between each quartet in the cc
+  * This causes pain when you use Selenium. There are a few stack overflow posts- but nothing really useful.
+  * This pausing also seems to be necessary to make PayPal work properly,
+  * without this it starts to complain about invalid credentials after only
+  * a few characters.
+  * */
+  def setValueSlowly(q: Query, value: String): Unit = {
+    for {
+      c <- value
+    } yield {
+      setValue(q, c.toString)
+      Thread.sleep(100)
     }
   }
 
