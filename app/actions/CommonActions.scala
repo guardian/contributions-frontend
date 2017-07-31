13268e728c723eb6d85a9b8bc52a71bee8e51fe5
Michael McNamara <michael.mcnamara@guardian.co.uk>
2017-07-31 18:11:48 +0100

    better identification to make logging more meaningful

--- app/actions/CommonActions.scala
+++ app/actions/CommonActions.scala
@@ -56,11 +56,14 @@ object CommonActions {
   implicit class MobileSupportRequest[A](val request: Request[A]) extends AnyVal {
 
     private def _platform = request.getQueryString("platform").orElse(request.session.get("platform")).getOrElse("web")
+    private def _sessionId = request.cookies.get("PLAY_SESSION").map(_.value).getOrElse("unknown")
 
     def platform: String = request.body match {
       case body: ContributionRequest => body.platform.getOrElse(_platform)
       case _ => _platform
     }
+
+    def sessionId: String = _sessionId
 
     def isAndroid: Boolean = platform == "android"
     def isIos: Boolean = platform == "ios"
