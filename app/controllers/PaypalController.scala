6a5d9d9d7f9aac1d31524fcb24deb2b76a8f91b3
Mohammad Haque <mohammad.haque@guardian.co.uk>
2017-08-01 17:59:50 +0100

    Implemented Paypal Capture feature to process payment from mobile client

--- app/controllers/PaypalController.scala
+++ app/controllers/PaypalController.scala
@@ -12,7 +12,7 @@ import models._
 import monitoring._
 import play.api.libs.ws.WSClient
 import play.api.mvc._
-import services.PaymentServices
+import services.{PaypalService, PaymentServices}
 import play.api.data.Form
 import utils.MaxAmount
 import play.api.libs.json._
@@ -28,6 +28,45 @@ import scala.util.Try
 class PaypalController(paymentServices: PaymentServices, checkToken: CSRFCheck, cloudWatchMetrics: CloudWatchMetrics)(implicit ec: ExecutionContext)
   extends Controller with Redirect with TagAwareLogger with LoggingTagsProvider {
   import ContribTimestampCookieAttributes._
+
+
+  case class CaptureRequest (
+    paymentId: String,
+    platform: String,
+    idUser: Option[IdentityId],
+    cmp: Option[String],
+    intCmp: Option[String],
+    refererPageviewId: Option[String],
+    refererUrl: Option[String],
+    ophanPageviewId: Option[String],
+    ophanBrowserId: Option[String])
+
+  object CaptureRequest {
+    implicit val jf: Format[CaptureRequest] = Json.format[CaptureRequest]
+  }
+
+  def capturePayment = (NoCacheAction andThen ABTestAction).async(parse.json[CaptureRequest]) { implicit request =>
+    val captureBody = request.body
+    val paypalService: PaypalService = paymentServices.paypalServiceFor(request)
+
+    paypalService.capturePayment(captureBody.paymentId)
+      .flatMap { payment =>
+        paypalService.storeMetaData(
+          paymentId = payment.getId,
+          testAllocations = request.testAllocations,
+          cmp = captureBody.cmp,
+          intCmp = captureBody.intCmp,
+          refererPageviewId = captureBody.refererPageviewId,
+          refererUrl = captureBody.refererUrl,
+          ophanPageviewId = captureBody.ophanPageviewId,
+          ophanBrowserId = captureBody.ophanBrowserId,
+          idUser = captureBody.idUser,
+          platform = Some(captureBody.platform),
+          ophanVisitId = None
+        )
+      }
+      .fold({_ => InternalServerError}, {_ => Ok})
+  }
 
   def executePayment(
     countryGroup: CountryGroup,
