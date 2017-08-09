97eb1d391c8070dbc3cb92d629e0349409d82759
Sam Desborough <sam.desborough@gmail.com>
2017-08-09 16:26:37 +0100

    Fix scroll bug on frontend caused by focussing on button

--- assets/javascripts/src/components/inline-payment/AmountButton.jsx
+++ assets/javascripts/src/components/inline-payment/AmountButton.jsx
@@ -3,13 +3,17 @@ import { h, Component} from 'preact';
 
 export default class AmountButton extends Component {
     componentDidMount() {
-        if (this.props.focusOnMount) this.base.focus();
+        if (this.props.focusOnMount) {
+            this.props.setAmount();
+        };
     }
 
     render(props) {
         return <button type="button"
-                       class={`contributions-inline-epic__button--amount ${props.dim && 'fade-out'}`}
-                       onFocus={props.setAmount}>
+                       class={`contributions-inline-epic__button--amount
+                               ${props.dim && 'fade-out'}
+                               ${(!props.dim && props.isSelected) && 'selected'}`}
+                       onClick={props.setAmount}>
             {props.symbol}{props.amount}
         </button>;
     }
