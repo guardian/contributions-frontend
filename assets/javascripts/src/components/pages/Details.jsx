f7cf5d26b3978017ea2f63628c30af4b838be866
jranks123 <jonathan.rankin@guardian.co.uk>
2018-01-09 15:06:46 +0000

    move marketing opt in both Stripe and Paypal to last step

--- assets/javascripts/src/components/pages/Details.jsx
+++ assets/javascripts/src/components/pages/Details.jsx
@@ -20,13 +20,6 @@ export default class Details extends React.Component {
                         value={this.props.details.postcode}
                         onChange={ event => this.props.updateDetails({ postcode: event.target.value })}
                         />
-
-            <div className="giraffe-checkbox">
-                <input id="guardian-opt-in" type="checkbox" name="guardian-opt-in"
-                       checked={this.props.details.optIn}
-                       onChange={event => this.props.updateDetails({ optIn: event.target.checked })} />
-                <label htmlFor="guardian-opt-in">Check this box to rec</label>
-            </div>
         </div>
     }
 }
